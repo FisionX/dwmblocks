d847686852c207bb48b94dda6bdf894fad4df020
Fernando <fherpato8@gmail.com>
2021-10-27 16:21:40 -0600

    Modified update intervals

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   {"Kbr:", "setxkbmap -query | awk '/layout/{ print $2 }'",            0,  11},
-  {"Net:", "cat /sys/class/net/eno1/operstate",                       30,   0},
+  {"Net:", "cat /sys/class/net/eth0/operstate",                       10,   0},
   {"Bat:", "cat /sys/class/power_supply/BAT0/capacity",               30,   0},
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
   {"Vol:", "pamixer --get-volume-human",                               0,  10},
-	{"", "date '+%b %d (%a) %I:%M%p'",                                   5,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",                                  30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
